11584da1263d1a864993aef91b0d559717356d57
Bekhruz <ruziboev1200@gmail.com>
2026-07-23 14:52:40 +0500

    Etiketka: chop siljishi standart -5mm

--- static/sw.js
+++ static/sw.js
@@ -4,7 +4,7 @@
 //   - HTML pages: network-first, fallback to cache for offline mode
 //   - API/POST: always network (don't cache mutations)
 
-const CACHE_NAME = 'yurit-v164';
+const CACHE_NAME = 'yurit-v165';
 const STATIC_ASSETS = [
   '/static/img/icon-192.png',
   '/static/img/icon-512.png',
